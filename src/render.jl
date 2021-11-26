ee39842bb7df227a19586728558baaf185d9f444
Vivek Srikrishnan <vs498@cornell.edu>
2021-11-26 06:52:06 -0500

    set up workflow to build pdf of notes

--- src/render.jl
+++ src/render.jl
@@ -5,22 +5,23 @@ Pkg.instantiate()
 using Mustache
 using Glob
 
-tokens = Dict()
+function create_remark_html(folder)
+    # load template
+    tpl = Mustache.load("template.html")
 
-# load template
-tpl = Mustache.load("template.html")
-
-lecture_folders = filter(isdir, readdir(glob"lecture*"))
-for (index, folder) in enumerate(lecture_folders)
     # load content
-    contentfile = open(joinpath(folder, string(folder, ".md")), "r")
+    contentfile = open(joinpath(folder, "content.md"), "r")
     content = read(contentfile, String)
 
-    title = string("Lecture", " ", index) 
+    title = string("Lecture", " ", last(folder, 2), " ", "Notes") 
 
     d = Dict("title" => title, "content" => content)
 
     outfile = open(joinpath(folder, string(folder, ".html")), "w")
     write(outfile, Mustache.render(tpl, d))
     close(outfile)
+
+    return nothing
 end
+
+create_remark_html(ARGS[1])
