2efca89328e35ce162b34aa97e6d25bdc7e929b7
Vivek Srikrishnan <vs498@cornell.edu>
2022-07-29 17:12:57 -0400

    move built files to parent directory

--- src/render.jl
+++ src/render.jl
@@ -10,4 +10,14 @@ note_paths = setdiff(filter(x -> isdir(x), readdir(".")), paths_ignore)
 for path in note_paths
     path
     Remark.slideshow(path, title = titlecase(replace(path, "-" => " ")), options = Dict("ratio" => "16:9", "highlightStyle" => "github", "highlightLanguage" => "julia"))
+    cd(path)
+    mv("build/index.html", "index.html")
+    mv("build/fonts", "fonts")
+    for f in filter(x -> occursin("js", x), readdir("build"))
+        mv(string("build/", f), f)
+    end
+    for f in filter(x -> occursin("css", x), readdir("build"))
+        mv(string("build/", f), f)
+    end
+    cd("..)
 end
